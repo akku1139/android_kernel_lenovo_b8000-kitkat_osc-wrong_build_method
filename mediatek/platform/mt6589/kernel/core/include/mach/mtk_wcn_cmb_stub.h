2212bc00d5d364b40f685b6bbdc8afab2d19cb45
-akku- <akkun11.open@gmail.com>
2024-12-20 01:40:17 +0900

    Import missing defs from: https://github.com/bq/aquaris-5HD/blob/1b08ad7554a863adb3eb2f8ec255c46f82571d94/mediatek/platform/mt6589/kernel/core/include/mach/mtk_wcn_cmb_stub.h

--- mediatek/platform/mt6589/kernel/core/include/mach/mtk_wcn_cmb_stub.h
+++ mediatek/platform/mt6589/kernel/core/include/mach/mtk_wcn_cmb_stub.h
@@ -54,15 +54,36 @@ typedef enum {
     CMB_STUB_AIF_CTRL_MAX = 2,
 } CMB_STUB_AIF_CTRL;
 
+typedef enum {
+    COMBO_FUNC_TYPE_BT = 0,
+    COMBO_FUNC_TYPE_FM = 1,
+    COMBO_FUNC_TYPE_GPS = 2,
+    COMBO_FUNC_TYPE_WIFI = 3,
+    COMBO_FUNC_TYPE_WMT = 4,
+    COMBO_FUNC_TYPE_STP = 5,
+    COMBO_FUNC_TYPE_NUM = 6
+} COMBO_FUNC_TYPE;
+
+typedef enum {
+    COMBO_IF_UART = 0,
+    COMBO_IF_MSDC = 1,
+    COMBO_IF_BTIF = 2,
+    COMBO_IF_MAX,
+} COMBO_IF;
+
 typedef void (*wmt_bgf_eirq_cb)(void);
 typedef int (*wmt_aif_ctrl_cb)(CMB_STUB_AIF_X, CMB_STUB_AIF_CTRL);
 typedef void (*wmt_func_ctrl_cb)(unsigned int, unsigned int);
+typedef signed long (*wmt_thermal_query_cb)(void);
+typedef int (*wmt_deep_idle_ctrl_cb)(unsigned int);
 
 typedef struct _CMB_STUB_CB_ {
     unsigned int size; //structure size
     /*wmt_bgf_eirq_cb bgf_eirq_cb;*//* remove bgf_eirq_cb from stub. handle it in platform */
     wmt_aif_ctrl_cb aif_ctrl_cb;
     wmt_func_ctrl_cb func_ctrl_cb;
+    wmt_thermal_query_cb thermal_query_cb;
+    wmt_deep_idle_ctrl_cb deep_idle_ctrl_cb;
 } CMB_STUB_CB, *P_CMB_STUB_CB;
 
 /*******************************************************************************
