a437f60e99bf9f9fb18425ae73f5b00d4705a6c4
-akku- <151517253+akku1139@users.noreply.github.com>
2024-12-20 00:14:31 +0900

    remove accdet_custom_def.h include
    probably accdet_custom_def.h was merged into accdet_custom.h

--- mediatek/platform/mt6589/kernel/drivers/accdet/accdet_hal.h
+++ mediatek/platform/mt6589/kernel/drivers/accdet/accdet_hal.h
@@ -35,7 +35,7 @@
 #include <mach/mt_typedefs.h>
 #include <mach/mt_reg_base.h>
 #include <mach/irqs.h>
-#include <accdet_custom_def.h>
+//#include <accdet_custom_def.h>
 #include <accdet_custom.h>
 #include <mach/reg_accdet.h>
 
