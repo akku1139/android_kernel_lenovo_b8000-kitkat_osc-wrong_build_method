7b696096bfae5003c164a84c9c32e72e4eb2f21b
-akku- <akkun11.open@gmail.com>
2024-12-26 18:12:18 +0900

    testing: --allow-multiple-definition

--- kernel/build.sh
+++ kernel/build.sh
@@ -6,7 +6,7 @@ release="n"
 rebuild="n"
 clean="n"
 makeflags="-w"
-makedefs="V=0"
+makedefs="V=0 KBUILD_LDFLAGS=--allow-multiple-definition"
 makejobs=${MAKEJOBS}
 curdir=`pwd`
 if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ];then
