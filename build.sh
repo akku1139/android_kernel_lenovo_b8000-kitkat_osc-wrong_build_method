621ae6a6eb0c21a639a9a2606813c05e9584a74c
-akku- <akkun11.open@gmail.com>
2025-04-13 17:08:56 +0900

    add project lenovo89_tb_x10_kk

--- build.sh
+++ build.sh
@@ -11,15 +11,15 @@ export MTK_ROOT_CUSTOM="$IT/mediatek/custom/"
 
 echo "**** Cleaning kernel tree ****"
 cd kernel
-TARGET_PRODUCT=lenovo89_tb_x10_jb2 make mrproper
+TARGET_PRODUCT=lenovo89_tb_x10_kk make mrproper
 cd ..
 
 #cd kernel
-#bash -e build.sh lenovo89_tb_x10_jb2
+#bash -e build.sh lenovo89_tb_x10_kk
 #cd ..
 
 echo "**** Building kernel ****"
-./makeMtk -t lenovo89_tb_x10_jb2 n k
+./makeMtk -t lenovo89_tb_x10_kk n k
 
 echo "**** Successfully built kernel ****"
 
